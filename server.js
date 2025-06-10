d189a2d3b5e435a4db0b3a62a0157cb167636248
ogstevyn <secondsteveoyebamiji@gmail.com>
2025-06-10 12:24:11 +0100

    database updated

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+
+//db connection
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Database connection error :", error);
+    process.exit(1);
+  }
+};
 
 //Middlewares
 app.use(express.json());
@@ -12,10 +24,21 @@ app.get("/", (req, res) => {
   res.json({
     message: "Hello World!",
     environment: process.env.NODE_ENV,
+    database: "Connected to MongoDB",
   });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`app is listening on ${PORT}`);
-});
+//Start Server
+const startServer = async () => {
+  try {
+    await connectDB();
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`app is listening on ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server: ", error);
+  }
+};
+
+startServer();
